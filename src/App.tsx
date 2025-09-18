be858b83bc043bd8a51ba663e72c80c2aa960067
PhoenixFan86 <johndoe@example.comphoenixfan86@gmail.com>
2025-09-19 00:18:04 +0300

    change path

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import About from "./components/About";
-import Header from "./components/header";
+import Header from "./components/Header";
 import Whatis from "./components/Whatis";
 
 function App() {
